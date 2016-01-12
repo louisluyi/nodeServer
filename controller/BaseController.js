046b1a0b2c58f336855880cb7f44180d97fba9bd
陆艺 <luyi@qipeipu.com>
2016-01-12 18:38:28 +0800

    第一次提交

---
+++ controller/BaseController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by luyi on 2016/1/12.
+ */
+export default class{
+
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by luyi on 2016/1/12.
+ */
+function foo(x, y = 5) {
+    console.log(x, y);
+}
+foo(1);
